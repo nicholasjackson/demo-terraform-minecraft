a54f5ccc50d90eb3184862d5c155ad06960c61ba
Nicholas Jackson <jackson.nic@gmail.com>
2023-09-06 16:02:35 +0100

    Update init.sh

--- terraform/gcp/core/scripts/init.sh
+++ terraform/gcp/core/scripts/init.sh
@@ -1,7 +1,10 @@
 #! /bin/bash -e
+apt update
+apt install software-properties-common
+
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && apt-get install boundary-enterprise -y
+apt update && apt-get install boundary-enterprise -y
 
 mkdir -p /etc/boundary/worker
 
