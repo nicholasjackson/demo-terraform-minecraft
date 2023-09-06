1a1386244b2000f55760717df1033d70d283843b
Nicholas Jackson <jackson.nic@gmail.com>
2023-09-06 16:07:11 +0100

    Update init.sh

--- terraform/gcp/core/scripts/init.sh
+++ terraform/gcp/core/scripts/init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 apt update
-apt install software-properties-common
+apt install software-properties-common -y
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
