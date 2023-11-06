7bc226179ea808fb3bf23af83cb53478ca19d540
Nic Jackson <jackson.nic@gmail.com>
2023-11-06 10:19:30 +0000

    Add secrets to deployment

--- terraform/azure/app/k8s_deployment.tf
+++ terraform/azure/app/k8s_deployment.tf
@@ -3,32 +3,28 @@
 # The key is the path to mount the file with _ substituted for /
 locals {
   config_files = {
-    "banned-ips.json"             = "${file("${path.module}/config/banned-ips.json")}"
-    "banned-players.json"         = "${file("${path.module}/config/banned-players.json")}"
-    "bukkit.yml"                  = "${file("${path.module}/config/bukkit.yml")}"
-    "ops.json"                    = "${file("${path.module}/config/ops.json")}"
-    "usercache.json"              = "${file("${path.module}/config/usercache.json")}"
-    "whitelist.json"              = "${file("${path.module}/config/whitelist.json")}"
-    "databases.json"              = "${file("${path.module}/config/databases.json")}"
-    "webservers.json"             = "${file("${path.module}/config/webservers.json")}"
-    "bluemap_core.conf"           = "${file("${path.module}/config/core.conf")}"
-    "bluemap_maps_overworld.conf" = "${file("${path.module}/config/overworld.conf")}"
-    "bluemap_maps_nether.conf"    = "${file("${path.module}/config/nether.conf")}"
-    "bluemap_maps_end.conf"       = "${file("${path.module}/config/end.conf")}"
+    "banned-ips.json"     = "${file("${path.module}/config/banned-ips.json")}"
+    "banned-players.json" = "${file("${path.module}/config/banned-players.json")}"
+    "bukkit.yml"          = "${file("${path.module}/config/bukkit.yml")}"
+    "ops.json"            = "${file("${path.module}/config/ops.json")}"
+    "usercache.json"      = "${file("${path.module}/config/usercache.json")}"
+    "whitelist.json"      = "${file("${path.module}/config/whitelist.json")}"
+    "databases.json"      = "${file("${path.module}/config/databases.json")}"
+    "webservers.json"     = "${file("${path.module}/config/webservers.json")}"
   }
 }
 
 locals {
   deployment_env = {
-    "WORLD_CHECKSUM"  = file("./checksum.txt") // checksum for the minecraft world, forces redeploy when tar changes
-    "MODS_BACKUP"     = "https://github.com/nicholasjackson/demo-terraform-minecraft/releases/download/mods/mods.tar.gz" // location of mods to install
+    "WORLD_CHECKSUM"  = file("./checksum.txt")                                                                                          // checksum for the minecraft world, forces redeploy when tar changes
+    "MODS_BACKUP"     = "https://github.com/nicholasjackson/demo-terraform-minecraft/releases/download/mods/mods.tar.gz"                // location of mods to install
     "WORLD_BACKUP"    = "https://github.com/nicholasjackson/demo-terraform-minecraft/releases/download/${var.environment}/world.tar.gz" // location of the minecraft world
-    "VAULT_ADDR"      = var.vault_addr // address of the vault server used by the Vault Lock Block
-    "VAULT_NAMESPACE" = "admin/${var.environment}" // namespace to use for the Vault Lock Block
-    "HASHICRAFT_env"  = var.environment // Vault namespace environment to use for the Vault Lock Block
-    "SPAWN_ANIMALS"   = "true" // enable animals
-    "SPAWN_NPCS"      = "true" // enable NPCs
-    "ONLINE_MODE"     = "false" // disable online mode
+    "VAULT_ADDR"      = var.vault_addr                                                                                                  // address of the vault server used by the Vault Lock Block
+    "VAULT_NAMESPACE" = "admin/${var.environment}"                                                                                      // namespace to use for the Vault Lock Block
+    "HASHICRAFT_env"  = var.environment                                                                                                 // Vault namespace environment to use for the Vault Lock Block
+    "SPAWN_ANIMALS"   = "true"                                                                                                          // enable animals
+    "SPAWN_NPCS"      = "true"                                                                                                          // enable NPCs
+    "ONLINE_MODE"     = "false"                                                                                                         // disable online mode
   }
 }
 
@@ -38,6 +34,26 @@ resource "kubernetes_config_map" "config" {
   }
 
   data = local.config_files
+}
+
+// Read from the dynamic secrets engine in vault and set a kubernetes secret
+// this does not keep the secret up to date and will only be update when the application is 
+// deployed. Better than using static database credentials but not ideal.
+data "vault_generic_secret" "sql_writer" {
+  path = "${vault_database_secrets_mount.minecraft.path}/creds/writer"
+}
+
+resource "kubernetes_secret" "db_writer" {
+  metadata {
+    name = "minecraft-db-${var.environment}"
+  }
+
+  data = {
+    db_host     = "${azurerm_postgresql_server.example.fqdn}:5432"
+    db_username = "${data.vault_generic_secret.sql_writer.data.username}@${azurerm_postgresql_server.example.name}"
+    db_password = data.vault_generic_secret.sql_writer.data.password
+    db_database = azurerm_postgresql_database.example.name
+  }
 }
 
 resource "kubernetes_deployment" "minecraft" {
@@ -118,6 +134,12 @@ resource "kubernetes_deployment" "minecraft" {
               read_only  = false
             }
           }
+
+          volume_mount {
+            name       = "db-secrets"
+            mount_path = "/etc/db_secrets"
+            read_only  = true
+          }
         }
 
         volume {
@@ -138,6 +160,14 @@ resource "kubernetes_deployment" "minecraft" {
 
           }
         }
+
+        volume {
+          name = "db-secrets"
+          secret {
+            secret_name = kubernetes_secret.db_writer.metadata.0.name
+          }
+        }
+
       }
     }
   }
